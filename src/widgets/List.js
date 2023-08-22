fc0dc30ebe7dff93471c20e4c8aec8071128d22f
Esraa Abduallah <esraahisham753@gmail.com>
2023-08-22 17:02:36 +0000

    Create list component & storybook

--- src/widgets/List.js
+++ src/widgets/List.js
@@ -15,6 +15,26 @@ const List = () => {
   return (
     <div>
       <h2>Items List</h2>
+      <ul>
+        {items.map(item => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
+      <form onSubmit={handleSubmit} data-testid="form">
+        <div className="form-group">
+          <input
+            type="text"
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+            id="newItem"
+            placeholder={inputPlaceholder}
+            className="form-control"
+          />
+        </div>
+        <input type="submit" value="add" className="btn btn-primary" />
+      </form>
     </div>
   );
 };
+
+export default List;
---
+++ src/widgets/List.stories.js
@@ -0,0 +1,10 @@
+import React from "react";
+import List from "./List";
+import "bootstrap/dist/css/bootstrap.css";
+
+export default {
+  component: List,
+  title: "List"
+};
+
+export const Default = () => <List />;
