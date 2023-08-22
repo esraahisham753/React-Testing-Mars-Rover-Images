61474efebc2a244267cd3b65195bb6ae07bf1217
Esraa Abduallah <esraahisham753@gmail.com>
2023-08-22 12:11:03 +0000

    Test DateSlider event using TestRenderer

---
+++ src/widgets/DateSlider.testrenderer.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import TestRenderer from "react-test-renderer";
+import DateSlider from "./DateSlider";
+import { solToDate } from "../services/sols";
+
+describe("DateSlider", () => {
+  describe("Change", () => {
+    it("Should display the date selected", () => {
+      const fn = jest.fn();
+      const tr = TestRenderer.create(
+        <DateSlider earth_date={"2017-5-13"} onDateChanged={fn} />
+      );
+      const input = tr.root.findByProps({ "data-testid": "date-slider" });
+      TestRenderer.act(() => {
+        input.props.onChange({ target: { value: "3877" } });
+      });
+      expect(fn.mock.calls).toEqual([[solToDate(3877)]]);
+    });
+  });
+});
